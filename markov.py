57f2bbb41ae4cc6100c8537199bb889777dc06db
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 12:11:31 -0700

    Finished make_text function. Fixed spacing issue. Added sys.argv functionality.

--- markov.py
+++ markov.py
@@ -1,5 +1,7 @@
+import sys
 from random import choice
 
+input_path = sys.argv[1]
 
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
@@ -47,49 +49,40 @@ def make_text(chains):
 
     text = ""
 
-    import random 
     #randomly choosing a key from chains dictionary and storing in random_key
     #random_key is returned one tuple
-    random_key = random.choice(chains.keys())
-    print random_key
+    random_key = choice(chains.keys())
+    # print random_key
     
     #unpacking string values from a tuple
     # key_word_1, key_word_2 = random_key[0], random_key[1]
     key_word_1, key_word_2 = random_key
-    # text += str(random_key[0][0]) + ' ' + str(random_key[0][1])
+    # text += str(random_key[0]) + ' ' + str(random_key[1])
     #concatenating unpacked string values to text variable, which is an empty string
     text += key_word_1 + ' ' + key_word_2 + ' '
-    print text
+    # print text
 
 
-    # random_key_unlisted = random_key[0] #since the tuple is in a string, need to unpack in order to use as a key for dictionary
     # random_key_values = chains[random_key[0]] #accessing the chains dictionary to find the corresponding value which is a list
     # random_value = random.choice(random_key_values)
-    # choosing a random item from the value list
-
+    
+    # while the key is a memeber of the chains dictionary...
     while random_key in chains:
+        # choosing a random item from the value list
+        random_value = choice(chains[random_key])
 
-        random_value = random.choice(chains[random_key])
-        print random_value
-
-        # adding the random string item to text
-        text += random_value
-        print text + ' ' ########WHY NOT ADDING SPACE AFTER 3RD WORD???????????#######
+        # adding the random string item to text plus space
+        text += random_value + ' '
         
         # creating new tuple with second word from the first key and the random word from the value list
         random_key = (random_key[1], random_value)
-        print random_key
-        # print chains[new_key]
-        # from the new tuple, grabbing the values from chains dictionary and randomly choosing an item from the value list
-        # new_random_value = random.choice(chains[new_key])
-        # print new_random_value
-        # second_new_key = (random_value, new_random_value)
-    print text
+        # if while loop continues, from the new tuple, grab the values from chains dictionary
+        # and randomly choosing an item from the value list. Rinse and repeat.
+    
+    return text
 
 
-    # return random_key 
-
-input_path = "green-eggs.txt"
+# input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
@@ -98,9 +91,6 @@ input_text = open_and_read_file(input_path)
 chains = make_chains(input_text)
 
 # Produce random text
-# random_text = make_text(chains)
+random_text = make_text(chains)
 
-# print random_text
-
-open_and_read_file(input_path)
-make_text(chains)
+print random_text
